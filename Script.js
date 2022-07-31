342b9e5fbf11cc205b77f03a705e276803aa411b
KaranxSingh <96904099+KaranxSingh@users.noreply.github.com>
2022-07-31 23:47:07 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -13,12 +13,14 @@ function validate() {
     // username validation 
     if (uname.value == "") {
         document.getElementById("unameError").innerHTML = "Username can't be empty";
+        document.getElementById("unameError").style.color = "cyan";
         flag = 0;
     } else if (uname.value.length >= 3) {
         document.getElementById("unameError").innerHTML = "";
         flag = 1;
     } else {
         document.getElementById("unameError").innerHTML = "Minimum 3 characters required";
+        document.getElementById("unameError").style.color = "green";
         flag = 0;
     }
 
@@ -30,26 +32,44 @@ function validate() {
     } else{
         document.getElementById("emailError").innerHTML = "Email is not valid";
         flag = 0;
+        document.getElementById("emailError").style.color = "cyan";
     }
 
 
         // password validation
     if (pass.value == "") {
         document.getElementById("passError").innerHTML = "Password can't empty";
+        document.getElementById("passError").style.color = "cyan";
         flag = 0;
     }else if (pass.value.length >= 8) {
         document.getElementById("passError").innerHTML = "";
         flag = 1; 
     } else {
         document.getElementById("passError").innerHTML = "Your password is weak";
+        document.getElementById("passError").style.color = "cyan";
         flag = 0; 
     }
     
+        //confirm password
+     if (document.getElementById('pass').value == document.getElementById("cnfrmPass").value)
+     {
+         document.getElementById("cnfrmPassError").innerHTML="";
+          }
+    else{
+        document.getElementById("cnfrmPassError").innerHTML=" Not matching";
+        document.getElementById("cnfrmPassError").style.color = "cyan";
+    }
+
+
+
+
+
     //phone number validation
     if (num.value.match(regex)) {
         document.getElementById("numError").innerHTML = "";
     } else {
         document.getElementById("numError").innerHTML = "Please Enter Number";
+        document.getElementById("numError").style.color = "cyan";
     }
 
 
